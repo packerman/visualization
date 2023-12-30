ef9c58567f8e1a28ee3557ad0ba88c7845e4c3d3
Piotr Ackermann <pioacker@gmail.com>
2023-12-30 03:00:09 +0100

    Matrix multiply refactor

--- src/commonMain/kotlin/framework/math/internal/matrix4.kt
+++ src/commonMain/kotlin/framework/math/internal/matrix4.kt
@@ -171,46 +171,41 @@ internal fun multiply(out: FloatArray, a: FloatArray, b: FloatArray): FloatArray
     val a32 = a[14]
     val a33 = a[15]
 
-    run {
-        val b0 = b[0]
-        val b1 = b[1]
-        val b2 = b[2]
-        val b3 = b[3]
-        out[0] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
-        out[1] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
-        out[2] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
-        out[3] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
-    }
-    run {
-        val b0 = b[4]
-        val b1 = b[5]
-        val b2 = b[6]
-        val b3 = b[7]
-        out[4] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
-        out[5] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
-        out[6] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
-        out[7] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
-    }
-    run {
-        val b0 = b[8]
-        val b1 = b[9]
-        val b2 = b[10]
-        val b3 = b[11]
-        out[8] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
-        out[9] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
-        out[10] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
-        out[11] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
-    }
-    run {
-        val b0 = b[12]
-        val b1 = b[13]
-        val b2 = b[14]
-        val b3 = b[15]
-        out[12] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
-        out[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
-        out[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
-        out[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
-    }
+    var b0 = b[0]
+    var b1 = b[1]
+    var b2 = b[2]
+    var b3 = b[3]
+    out[0] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
+    out[1] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
+    out[2] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
+    out[3] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
+
+    b0 = b[4]
+    b1 = b[5]
+    b2 = b[6]
+    b3 = b[7]
+    out[4] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
+    out[5] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
+    out[6] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
+    out[7] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
+
+    b0 = b[8]
+    b1 = b[9]
+    b2 = b[10]
+    b3 = b[11]
+    out[8] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
+    out[9] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
+    out[10] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
+    out[11] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
+
+    b0 = b[12]
+    b1 = b[13]
+    b2 = b[14]
+    b3 = b[15]
+    out[12] = b0 * a00 + b1 * a10 + b2 * a20 + b3 * a30
+    out[13] = b0 * a01 + b1 * a11 + b2 * a21 + b3 * a31
+    out[14] = b0 * a02 + b1 * a12 + b2 * a22 + b3 * a32
+    out[15] = b0 * a03 + b1 * a13 + b2 * a23 + b3 * a33
     return out
 }
 
